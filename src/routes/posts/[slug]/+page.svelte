cfb2df127fd3ad592c7aa85906d3f1cbce5aeb74
josean-dev <youtube@josean.com>
2023-09-17 01:55:24 -0400

    Changes

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -12,21 +12,20 @@
 		let pres: HTMLCollection = document.getElementsByTagName('pre');
 		for (let _ of pres) {
 			const pre = _ as HTMLPreElement;
-			if (![...pre.classList].some((el) => el.startsWith('language-'))) {
-				continue;
-			}
-			const preParent = pre.parentNode;
+			if ([...pre.classList].some((el) => el.startsWith('language-'))) {
+				const preParent = pre.parentNode;
 
-			const wrapper = document.createElement('div');
-			wrapper.className = 'relative';
+				const wrapper = document.createElement('div');
+				wrapper.className = 'relative';
 
-			new CopyCodeButton({
-				target: wrapper
-			});
+				new CopyCodeButton({
+					target: wrapper
+				});
 
-			if (preParent) {
-				preParent.replaceChild(wrapper, pre);
-				wrapper.appendChild(pre);
+				if (preParent) {
+					preParent.replaceChild(wrapper, pre);
+					wrapper.appendChild(pre);
+				}
 			}
 		}
 	});
