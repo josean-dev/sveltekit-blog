51fc5681fed95f1e9b26db0be513cb735208ed21
josean-dev <youtube@josean.com>
2024-07-12 14:07:01 -0400

    Implemented H1 & HeadingContainer components

---
+++ src/lib/components/headings/H1.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { cn } from "$lib/utils/tailwindcss";
+
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  interface $$Props extends Partial<HTMLHeadingElement> {}
+</script>
+
+<h1
+  {...$$restProps}
+  class={cn("font-bold text-4xl", $$restProps.class)}
+>
+  <slot />
+</h1>
---
+++ src/lib/components/headings/HeadingContainer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { cn } from "$lib/utils/tailwindcss";
+
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  interface $$Props extends Partial<HTMLHeadingElement> {}
+</script>
+
+<div
+  {...$$restProps}
+  class={cn(
+    "p-4 flex items-center justify-between",
+    $$restProps.class
+  )}
+>
+  <slot />
+  <slot name="button" />
+</div>
