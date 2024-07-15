03f6f04d4460234670a1d352669ed7aec8465006
josean-dev <youtube@josean.com>
2024-07-15 16:47:29 -0400

    Added LoadingSpinnerIcon

---
+++ src/lib/components/svg/LoadingSpinnerIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let klass: string = "";
+
+  export { klass as class };
+</script>
+
+<svg
+  class={klass}
+  xmlns="http://www.w3.org/2000/svg"
+  x="0px"
+  y="0px"
+  viewBox="0 0 32 32"
+>
+  <path
+    d="M 15 4 L 15 10 L 17 10 L 17 4 L 15 4 z M 7 7 L 7 9 L 9 9 L 9 7 L 7 7 z M 9 9 L 9 11 L 11 11 L 11 9 L 9 9 z M 23 7 L 23 9 L 25 9 L 25 7 L 23 7 z M 23 9 L 21 9 L 21 11 L 23 11 L 23 9 z M 4 15 L 4 17 L 10 17 L 10 15 L 4 15 z M 22 15 L 22 17 L 28 17 L 28 15 L 22 15 z M 9 21 L 9 23 L 11 23 L 11 21 L 9 21 z M 9 23 L 7 23 L 7 25 L 9 25 L 9 23 z M 21 21 L 21 23 L 23 23 L 23 21 L 21 21 z M 23 23 L 23 25 L 25 25 L 25 23 L 23 23 z M 15 22 L 15 28 L 17 28 L 17 22 L 15 22 z"
+  />
+</svg>
