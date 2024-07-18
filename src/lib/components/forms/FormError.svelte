eb34d80a0d7eaa06a2f79860fdb57fb4b6c49110
josean-dev <youtube@josean.com>
2024-07-18 11:13:15 -0400

    Added component for Form level errors

---
+++ src/lib/components/forms/FormError.svelte
@@ -0,0 +1,8 @@
+<script>
+  import RoundErrorIcon from "../svg/RoundErrorIcon.svelte";
+</script>
+
+<div class="flex items-center space-x-1 text-red-500 font-bold py-2">
+  <RoundErrorIcon class="size-5 fill-current" />
+  <span><slot /></span>
+</div>
---
+++ src/lib/components/svg/RoundErrorIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let klass: string = "";
+
+  export { klass as class };
+</script>
+
+<svg
+  class={klass}
+  xmlns="http://www.w3.org/2000/svg"
+  x="0px"
+  y="0px"
+  viewBox="0 0 24 24"
+>
+  <path
+    d="M12,2C6.477,2,2,6.477,2,12c0,5.523,4.477,10,10,10s10-4.477,10-10C22,6.477,17.523,2,12,2z M13,17h-2v-2h2V17z M13,13h-2 V7h2V13z"
+  />
+</svg>
