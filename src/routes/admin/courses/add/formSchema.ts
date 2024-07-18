bbc6c64d9060f007f37532b5e31b3adf03f088cd
josean-dev <youtube@josean.com>
2024-07-18 11:16:39 -0400

    Modified course add formSchema to make name/slug required

--- src/routes/admin/courses/add/formSchema.ts
+++ src/routes/admin/courses/add/formSchema.ts
@@ -2,8 +2,13 @@ import { z } from "zod";
 
 // Define outside the load function so the adapter can be cached
 export const schema = z.object({
-  name: z.string(),
-  slug: z.string().regex(/^[a-z0-9]+(-[a-z0-9]+)*$/, {
-    message: "Slug must be lowercase words/digits separated by dashes"
-  })
+  name: z.string().trim().min(1, "Name is required"),
+  slug: z
+    .string()
+    .trim()
+    .min(1, "Slug is required")
+    .regex(/^[a-z0-9]+(-[a-z0-9]+)*$/, {
+      message:
+        "Slug must be lowercase words/digits separated by dashes"
+    })
 });
