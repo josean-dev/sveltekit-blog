41449a6764222ffbc610be3cf2d7cbdf72a11460
josean-dev <youtube@josean.com>
2024-07-18 10:19:45 -0400

    Modified Td component to support link

--- src/lib/components/table/Td.svelte
+++ src/lib/components/table/Td.svelte
@@ -3,15 +3,32 @@
   import type { HTMLTdAttributes } from "svelte/elements";
 
   /* eslint-disable @typescript-eslint/no-unused-vars */
-  interface $$Props extends Partial<HTMLTdAttributes> {}
+  interface $$Props extends Partial<HTMLTdAttributes> {
+    href?: string;
+  }
+
+  export let href: string = "";
+
+  let klass: string | undefined | null = "";
+
+  export { klass as class };
+
+  const classes = `block px-6 py-4 dark:text-gray-300 text-gray-800 
+                   border-b dark:border-gray-700
+                   ${klass}`;
 </script>
 
 <td
+  class={cn({
+    classes: !href
+  })}
   {...$$restProps}
-  class={cn(
-    "px-6 py-4 dark:text-gray-300 text-gray-800 border-b dark:border-gray-700",
-    $$restProps.class
-  )}
 >
-  <slot />
+  {#if href}
+    <a {href} class={classes}>
+      <slot />
+    </a>
+  {:else}
+    <slot />
+  {/if}
 </td>
