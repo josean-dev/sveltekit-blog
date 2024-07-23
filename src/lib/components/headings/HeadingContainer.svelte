0185e9db0b146822274b709e36618707acee5120
josean-dev <youtube@josean.com>
2024-07-22 20:36:29 -0400

    Added underline option to HeadingContainer component

--- src/lib/components/headings/HeadingContainer.svelte
+++ src/lib/components/headings/HeadingContainer.svelte
@@ -2,15 +2,20 @@
   import { cn } from "$lib/utils/tailwindcss";
 
   /* eslint-disable @typescript-eslint/no-unused-vars */
-  interface $$Props extends Partial<HTMLHeadingElement> {}
+  interface $$Props extends Partial<HTMLHeadingElement> {
+    underline?: boolean;
+  }
+
+  export let underline = false;
 </script>
 
 <div
   {...$$restProps}
-  class={cn(
-    "p-4 flex items-center justify-between",
-    $$restProps.class
-  )}
+  class={cn({
+    "p-4 flex items-center justify-between": true,
+    "border-b dark:border-gray-700": underline,
+    [$$restProps.class]: true
+  })}
 >
   <slot />
   <slot name="button" />
