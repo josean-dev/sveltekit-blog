b8a2acec6bdc3b883683ead0cdafbb94850f5a23
josean-dev <youtube@josean.com>
2024-07-22 20:36:13 -0400

    Created H2 component

---
+++ src/lib/components/headings/H2.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { cn } from "$lib/utils/tailwindcss";
+
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  interface $$Props extends Partial<HTMLHeadingElement> {}
+
+  let klass: string | undefined | null = "";
+
+  export { klass as class };
+</script>
+
+<h2 class={cn("font-semibold text-3xl", klass)} {...$$restProps}>
+  <slot />
+</h2>
