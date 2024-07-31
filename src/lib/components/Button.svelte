5858b2e7fbaabcc4bee9da0d0e123911b7418b45
josean-dev <youtube@josean.com>
2024-07-31 17:19:47 -0400

    Added purple color option to Button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -8,23 +8,32 @@
   import LoadingSpinnerIcon from "./svg/LoadingSpinnerIcon.svelte";
 
   /* eslint-disable @typescript-eslint/no-unused-vars */
-  type $$Props = ButtonProps & { loading?: boolean };
+  type $$Props = ButtonProps & {
+    loading?: boolean;
+    color?: "blue" | "purple";
+  };
   type $$Events = ButtonEvents;
 
   let klass: string | undefined | null = "";
 
   export { klass as class };
 
+  export let color: "blue" | "purple" = "blue";
+
   export let loading = false;
 </script>
 
 <Button.Root
-  class={cn(
-    "relative block",
-    "px-3 py-2 bg-sky-500 hover:bg-sky-600 text-white rounded-md",
-    "font-medium",
-    klass
-  )}
+  class={cn({
+    "relative block": true,
+    "px-3 py-2 text-white rounded-md": true,
+    "font-medium": true,
+    "disabled:bg-slate-200 disabled:text-slate-400": true,
+    "dark:disabled:bg-slate-700 dark:disabled:text-slate-500": true,
+    "bg-sky-500 hover:bg-sky-600": color === "blue",
+    "bg-purple-500 hover:bg-purple-600": color === "purple",
+    [`${klass}`]: true
+  })}
   on:click
   on:keydown
   {...$$restProps}
