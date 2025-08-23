c23c39b6e4835f3b3282e1c7b82c65d254cf5506
josean-dev <youtube@josean.com>
2025-08-22 20:35:26 -0400

    Finished initial sveltekit better-auth implementation

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { Session, User } from "better-auth";
+
 // for information about these interfaces
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
+    interface Locals {
+      session: Session;
+      user: User;
+    }
     // interface PageData {}
     // interface Platform {}
   }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./server/db/client";
+import { sveltekitCookies } from "better-auth/svelte-kit";
+import { getRequestEvent } from "$app/server";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -8,11 +10,12 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true
-  }
+  },
   // socialProviders: {
   //   github: {
   //     clientId: process.env.GITHUB_CLIENT_ID as string,
   //     clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
   //   },
   // },
+  plugins: [sveltekitCookies(getRequestEvent)]
 });
